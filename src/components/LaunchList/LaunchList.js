12869efcb29ac662e8b2e5fa1b5ab0d02575841b
Volkan <volkantas@outlook.com>
2023-04-07 02:07:46 +0300

    feat: add launch list component

--- src/components/LaunchList/LaunchList.js
+++ src/components/LaunchList/LaunchList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import { useGetLaunchesQuery } from "../../services/launches";
+import './LaunchList.scss';
+import {useSelector} from "react-redux";
 
-const LaunchList = () => {
+export default function LaunchList({ onSelectLaunch }) {
+    const page = useSelector((state) => state.launches.page)
+    const { data: launches, isLoading } = useGetLaunchesQuery(page);
+
+    if (isLoading) {
+        return <div className="loading">Loading...</div>;
+    }
+
     return (
-        <div className="launch-list">
-            List
+        <div className="container">
+            {launches.docs.map((launch) => (
+                <div className='launchItem' key={launch.id} onClick={() => onSelectLaunch(launch)}>
+                    <img width={80} loading="lazy" src={launch.links.patch.small} alt={launch.name} />
+                    <span className='name'>{launch.name}</span>
+                </div>
+            ))}
         </div>
     );
-};
-
-export default LaunchList;
+}
