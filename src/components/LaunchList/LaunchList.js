a818ba057bd840ad12d44a952e338af49e748fcd
Volkan <volkantas@outlook.com>
2023-04-07 15:15:12 +0300

    feat: add list item navigation link

--- src/components/LaunchList/LaunchList.js
+++ src/components/LaunchList/LaunchList.js
@@ -1,10 +1,10 @@
-import { useGetLaunchesQuery } from "../../services/launches";
+import {useGetLaunchesQuery} from "../../services/launches";
 import './LaunchList.scss';
 import {useSelector} from "react-redux";
 
-export default function LaunchList({ onSelectLaunch }) {
+export default function LaunchList({onSelectLaunch}) {
     const {page, searchName} = useSelector((state) => state.launches)
-    const { data: launches, isLoading } = useGetLaunchesQuery({ name: searchName, page });
+    const {data: launches, isLoading} = useGetLaunchesQuery({name: searchName, page});
 
     if (isLoading || !launches) {
         return <div className="loading">Loading...</div>;
@@ -13,10 +13,10 @@ export default function LaunchList({ onSelectLaunch }) {
     return (
         <div className="container">
             {launches.docs.map((launch) => (
-                <div className='launchItem' key={launch.id} onClick={() => onSelectLaunch(launch)}>
-                    <img width={80} loading="lazy" src={launch.links.patch.small} alt={launch.name} />
+                <a href={`/spacex-launches/launch/${launch.id}`} className='launchItem' key={launch.id}>
+                    <img width={80} loading="lazy" src={launch.links.patch.small} alt={launch.name}/>
                     <span className='name'>{launch.name}</span>
-                </div>
+                </a>
             ))}
         </div>
     );
